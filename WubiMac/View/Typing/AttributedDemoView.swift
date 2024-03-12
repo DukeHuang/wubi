76f256f4a0670e095573a44c24f47ea2fe1a5e6b
huang.yongyou <huang.yongyou@westone.com.cn>
2024-03-12 19:08:09 +0800

    add typing module

---
+++ WubiMac/View/Typing/AttributedDemoView.swift
@@ -0,0 +1,18 @@
+//
+//  AttributedDemoView.swift
+//  WubiMac
+//
+//  Created by yongyou on 2024/3/12.
+//
+
+import SwiftUI
+
+struct AttributedDemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttributedDemoView()
+}
